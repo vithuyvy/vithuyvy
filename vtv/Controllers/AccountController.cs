3c7ae4eddd8e4a8d20071ca544e93256d2c82d87
VI THUY VY <1821050073@student.humg.edu.vn>
2021-04-11 23:59:02 +0700

    First Commit

---
+++ vtv/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using vtv.Models;
+
+namespace vtv.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login (string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AccountModel acc , string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if  (acc.Username == "admin" && acc.Password == "123123")
+                {
+                    FormsAuthentication.SetAuthCookie(acc.Username, true);
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+            return View(acc);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToLocal("Index" , "Home");
+        }
+        private ActionResult RedirectToLocal(string returnUrl, string v)
+        { if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+ 
+    }
+}
---
+++ vtv/Models/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace vtv.Models
+{
+    public class AccountModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
